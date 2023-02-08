0c1b2e586a719e21d353ffa64a277e0cc4af4acd
LuisMi01 <113381771+LuisMi01@users.noreply.github.com>
2023-02-08 11:31:47 +0000

    Ejercicio 1 Terminado

---
+++ Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int a = 3; 
+        System.out.println("a = " + a); 
+        int b = a + 5;
+        System.out.println("b = " + b);
+        a = 7;
+        System.out.println("a = " + a + " b = " + b);
+      
+    }
+    
+    
+}
